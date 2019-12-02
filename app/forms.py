0a77c017afed43b4cb0b54e302ace43f0ec574a5
f8upd8 <f8upd8@protonmail.com>
2019-12-02 20:31:22 +0700

    add forms.py, user form

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from wtforms import Form, StringField, PasswordField, validators
+
+class SignUpForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('E-mail', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
