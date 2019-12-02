15a2648a4696bec8215bcc7b773a5dcaa8689992
f8upd8 <f8upd8@protonmail.com>
2019-12-02 21:06:49 +0700

    fixed typo in models, added user model init

--- app/models.py
+++ app/models.py
@@ -12,11 +12,20 @@ class Article(Base):
         self.content = content
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<Article %r>' % self.id
+
 
 class User(Base):
     __tablename__ = 'Users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(20), unique=True)
+    username = Column(String(20), unique=True)
     password = Column(String(64), unique=True)
     email = Column(String(), unique=True)
+
+    def __init__(self, name, email, password):
+        self.username = name
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
