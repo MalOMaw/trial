ed63f316b235b938ce600a22c47d174a4705f65f
f8upd8 <f8upd8@protonmail.com>
2019-12-03 22:03:17 +0700

    added sign in form, adapted for PEP8

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from wtforms import Form, StringField, PasswordField, validators
 
+
 class SignUpForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('E-mail', [validators.Length(min=6, max=35)])
@@ -8,3 +9,10 @@ class SignUpForm(Form):
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
+
+
+class SignUpForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+        validators.DataRequired()
+    ])
